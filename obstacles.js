cef91650c933e318f821529e517a70216df3aab9
Jullie17 <jc10710@nyu.edu>
2021-12-18 17:28:15 -0500

    Obstacles/bonus/sound
    I added the image of the obstacles which were birds, boxes, and fences.
    Then, also added a sound effect when the bunny jump and when the game is over.
    For the carrots, I had added image of a carrot which is indicating a bonus points. So I made the carrot moved setting its velocity to make it move over time and the speed increases over time. However, when you collide with the carrot as much as the time you are colliding you get extra points by 0.5

--- obstacles.js
+++ obstacles.js
@@ -3,6 +3,7 @@ class Obstacles{
 	constructor(type, sprite){
 		this.type=random(0,3);
 		this.sprite = createSprite(width, 550, 100, 100);
+		this.sprite.addImage(loadImage('assets/box_obstacle.png'));
 		//this.sprite2 = createSprite(width, 550, 50, 200);
    		//this.sprite3 = createSprite(width, 300, 100, 50);
 	}
@@ -19,17 +20,25 @@ class Obstacles{
 		    this.sprite.position.y = 450;
 		    this.sprite.height = 300;
 		    this.sprite.width = 50;
+
+		    this.sprite.addImage(loadImage('assets/fence_obstacle.png')); // trying to put fence image for obstacles, but if I put the image here the fence obstacle repeats
 		    break;
 		  case 1:
 		    this.sprite.position.y = 500;
 		    this.sprite.height = 50;
 		    this.sprite.width = 100;
+
+		    this.sprite.addImage(loadImage('assets/bird_obstacle_2(1).png'));
+
 		    break;
 		  case 2:
 		    this.sprite.position.y = 550;
 		    this.sprite.height = 100;
 		    this.sprite.width = 100;
+
+		    this.sprite.addImage(loadImage('assets/box_obstacle.png'));
 		  	break;
+		  
 		}
 	}
 
--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let score =0;
 let o;
 let speed;
 
+
 //image
 let bg_1;
 let bunny;
@@ -34,7 +35,10 @@ let bunny_slide;
 
 //background music
 let bg_music;
+let jump_sound;
+let game_over;
 
+let bonus=0;
 
 function preload(){
   //bunny = loadAnimation('assets/bunny_001.png');
@@ -50,60 +54,68 @@ function preload(){
   //p.addImage(loadImage("bunny001.png"));
 
   bg_music = loadSound('assets/bg_music.mp3');
+  jump_sound = loadSound('assets/Bunny_jump_sound_effect.m4a');
+  game_over = loadSound('assets/game_over_sound_1.mp3');
 }
 
 function setup() {
   // put setup code here
   createCanvas(1280, 800);
-  //background(0);
-   //p = createSprite(width/3, 400, 100, 100); //p as a player
+  
 
-   //p.addImage('bunny', img_bunny);
-   //p = loadImage('assets/bunny_001.PNG', 100, 100);
-   //p = image(bunny, width/3, 400, 100, 100);
+   
   //p = createSprite(width/3, 400, 100, 100); //p as a player
   p = createSprite(width/3, 400, 200, 100); //p as a player
   p.addImage(img_bunny);
   //p.addImage(loadImage("bunny001.png"));
 
   g = createSprite(width/2, 700, width, 200); 
-  
-  //for bunny sleeping
-  //sleep = createSprite(w) 
-   o = new Obstacles(1,2);
-   /*o_one = createSprite(width, 550, 100, 100);
-   o_two = createSprite(width, 550, 50, 200);
-   o_three = createSprite(width, 300, 100, 50); */
-  //let list_of_obstacles = [o_one, o_two, o_three];
-  //let obstacle = random(list_of_obstacles);
+  g.addImage(loadImage('assets/grass.PNG'));
+
+
+  carrot = createSprite(width/3, 50, 100, 100);
+  carrot.addImage(loadImage('assets/carrot.png'));
+  carrot.velocity.x = map(score, 0, 50, 5, 30);
+
+  o = new Obstacles(1,2);
+
+
 
     // sprites to be visible
    //p.visible=false;
 
-   //g_font = loadFont('libraries/Robotron-0Wvvo.ttf')
+   
 
    bunny_fly = loadAnimation('assets/bunny_fly_001.png','assets/bunny_fly_004.png');
    p.addAnimation('jump', bunny_fly);
    //bunny_running = loadAnimation('assets/bunny_run_01.png');
    bunny_running = loadAnimation('assets/bunny_running_001.png','assets/bunny_running_006.png');
    p.addAnimation('run', bunny_running);
-   //bunny_sleep = loadAnimation('assets/bunny_sleeping_001.png', 'assets/bunny_sleeping_017.png');
+   //bunny_sleep = loadAnimation('assets/bunny_sleeping_001.png', 'assets/bunny_sleeping_017.png'); // moved to setup, and made this animation calling bunny_sleep and called them in the if statement where pause ==true
 
-   //bg_music.play();
-   bunny_slide = loadAnimation('assets/bunny_slide.PNG');
+   //bg_music.play(); // only playing the music once 
+   bunny_slide = loadAnimation('assets/bunny_slide.png');
    p.addAnimation('slide', bunny_slide);
 
 }
 
 function draw() {
-  if(bg_music.isPlaying()==false){
+
+  if(bg_music.isPlaying()==false && pause==false && end==false){
     bg_music.play();
   }
+
+  //speed -------------------------------increasing the speed to its max from 0 to 50---------
   o_speed = map(score, 0, 50, 10, 25);
-  p.debug=true;
+
+  // debug -----------------------shows the sprite size the rectangular boxes in order to recognize the collsion b/w the obstacles
+  //p.debug=true;
+
+  // game ---------------------------when it wasn't paused ---------------------------------------
   if(pause==false){
-    p.velocity.y+=GRAVITY;
+    p.velocity.y+=GRAVITY; // adding gravity to the player(bunny)
     
+
     image(bg_1, 0,0,width, height);
     //background(135, 206, 235);
     //p.collide(g);
@@ -126,6 +138,9 @@ function draw() {
       jumpCounter+=1;
       p.velocity.y= -jump; // - as it is jumping backwards
       p.changeAnimation('jump');
+      if(jump_sound.isPlaying()==false){
+        jump_sound.play();
+      }
     }
     if((o.sprite.position.x<0)){
       score+=1;
@@ -145,6 +160,20 @@ function draw() {
     o_two.position.x-=o_speed;
     o_three.position.x-=o_speed; */
 
+    if(score%1==0){
+      carrot.visible=true;
+    }else{
+      carrot.visible=false;
+    }
+    if(carrot.position.x > width){
+      carrot.position.x =0;
+    }
+
+    if(p.overlap(carrot)){
+      bonus+=0.5;
+    }
+    
+
     if(keyDown('DOWN')){
       p.setCollider("rectangle",0,25,100, 50);
       p_height = 50;
@@ -163,13 +192,42 @@ function draw() {
       fill(0,100,255);
       //rect(p.position.x, p.position.y, 100,p_height);
     }
-   
+
+    textFont(g_font);
+    noStroke();
+    fill(0);
+    textSize(30);
+    textAlign(LEFT, CENTER);
+    text("Bonus : "+bonus, 20, 80);
+
+    // ---------------------------------------text - score when game is still going 
+    textFont(g_font);
+    noStroke();
+    fill(0);
+    textSize(30);
+    textAlign(LEFT, CENTER);
+    text("YOUR SCORE : "+score, 20, 50);
+
+    //-------------------------------------text - how to jump and lside when game is still going on
+    textAlign(LEFT, CENTER);
+    if(score<5){
+    //space and down
+  
+    text("How to Jump? -> PRESS 'SPACE' ", 20, height-75);
+    text("How to SLIDE? -> PRESS 'DOWN(V)'", 20, height-25);
+
+    }
    
   }
   if(pause==true){
-    console.log(end);
+    //console.log(end);
     p.setSpeed(0.00001);
+
     if(end==true){
+      if(game_over.isPlaying()==false){
+        bg_music.pause();
+        game_over.play();
+      }
       //fade out background for pause screen
       strokeWeight(10);
       stroke(150, 80);
@@ -187,6 +245,7 @@ function draw() {
       textAlign(CENTER, CENTER);
       textSize(60);
       text("GAME OVER, BUNNY DEAD", width/2, height/2-50);
+      text("YOUR SCORE : "+(score+bonus), width/2, height/2 +50);
     }else{
 
       //fade out background for pause screen
@@ -198,7 +257,7 @@ function draw() {
       noFill();
 
       //bunny sleeping animation on the back
-      animation(bunny_sleep, width/2, height/2);
+      animation(bunny_sleep, width/2, height/2); // calling the sleeping animation and the x,y position for the animation
 
       textFont(g_font);
       noStroke();
@@ -207,10 +266,13 @@ function draw() {
       textSize(60);
       text("PAUSED, BUNNY SLEEPING", width/2, height/2-50);
     }
+
+    // text of indicating how to restart the game----------------------------------------------------
     textFont(g_font);
     textSize(30);
     text("Press R to RESTART", width/2, height/2);
 
+    // ------------------------------------------------pausing ----------------------------------------------
     if(keyWentDown('R')){
       //obstacle coming out again from the width
       o.reset();
@@ -219,14 +281,20 @@ function draw() {
       pause=false;
       end=false;
       score=0;
+      bonus=0;
     }
   }
+
+  // text used in the game
   textFont(g_font);
   noStroke();
   fill(0);
+
   textAlign(CENTER, CENTER);
   textSize(30);
   text("How to PAUSE? -> PRESS 'A' ", width-250, 25);
+
+  /*
   textAlign(LEFT, CENTER);
   if(score<5){
     //space and down
@@ -234,10 +302,10 @@ function draw() {
   text("How to Jump? -> PRESS 'SPACE' ", 20, height-75);
   text("How to SLIDE? -> PRESS 'DOWN(V)'", 20, height-25);
 
-  }
+  }*/
   
 
-  text("YOUR SCORE : "+score, 20, 50);
+  //text("YOUR SCORE : "+score, 20, 50);
 
   if(keyWentDown('A')&&end==false){
     if(pause==true){
